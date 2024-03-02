30f6564953aeb3bd46181dd4b43577c8169bcd75
NakaeKodai <0uz047526ky3k4d@au.com>
2024-03-02 22:26:10 +0900

    採取ポイントでのログ出し

---
+++ Assets/Script/Player/Harvest.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harvest : MonoBehaviour
+{
+    private PlayerInputAction playerInputAction;  //InputSystemを入れている変数 
+    private bool isEnter;
+
+    void Start()
+    {
+        playerInputAction = new PlayerInputAction();
+        playerInputAction.Enable();
+    }
+
+
+    void Update()
+    {
+        if(playerInputAction.Player.Fire.triggered && isEnter)
+        {
+            Debug.Log("採集");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Material"))
+        {
+            isEnter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Material"))
+        {
+            isEnter = false;
+        }
+    }
+}
