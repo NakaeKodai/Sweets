5d7569a27422199954eb37317d7960e345a17f53
FUNA1116 <153796025+FUNA1116@users.noreply.github.com>
2024-03-04 17:26:30 +0900

    文字送りのプログラム

--- Assets/Script/Player/Talk.cs
+++ Assets/Script/Player/Talk.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; //TextMeshProを使う場合は読み込みが必要
+using UnityEngine.SceneManagement;
 
 public class Talk : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class Talk : MonoBehaviour
     NPCState npcState;
 
     public GameObject talkWindow;
+    [SerializeField] private TextMeshProUGUI textWindow;
+    private bool windowStop = false;//画面に文字を表示させたかを判別
 
     void Start()
     {
@@ -29,25 +33,32 @@ public class Talk : MonoBehaviour
         if(playerInputAction.Player.Fire.triggered && isEnter && !isTalking)
         {
             talkPage = 0;
-            Text textWindow = talkWindow.GetComponent<Text>();
+            textWindow = talkWindow.GetComponent<TextMeshProUGUI>();
             textWindow.text = npcState.list[talkPage];
             talkWindow.SetActive(true);
+            if(!windowStop){
+                StartCoroutine(Simple());
+            }
             isTalking = true;
             interval = true;
         }
         playerInputAction.Player.Fire.canceled += ctx => interval = false;
         if(isTalking)
         {
-            if(playerInputAction.Player.Fire.triggered && !interval) talkPage++;
+            if(playerInputAction.Player.Fire.triggered && !interval){
+                talkPage++;
+                windowStop = false;
+            } 
             if(talkPage >= npcState.list.Count) 
             {
                 talkWindow.SetActive(false);
                 isTalking = false;
             }
-            else
+            else if(!windowStop)
             {
-                Text textWindow = talkWindow.GetComponent<Text>();
-                textWindow.text = npcState.list[talkPage];    
+                textWindow = talkWindow.GetComponent<TextMeshProUGUI>();
+                textWindow.text = npcState.list[talkPage];
+                StartCoroutine(Simple());    
             }
         }
     }
@@ -67,5 +78,21 @@ public class Talk : MonoBehaviour
         {
             isEnter = false;
         }
+    }
+
+    // 文字送りのプログラム
+    private IEnumerator Simple(){
+        textWindow.maxVisibleCharacters = 0;
+        windowStop = true;
+
+        for (var i = 0; i < textWindow.text.Length; i++)
+        {
+            yield return new WaitForSeconds(0.03f);//ここのかっこの中の数値を変えることで文字送りのスピードを変えられる(秒)
+
+            // 文字の表示数を増やしていく
+            textWindow.maxVisibleCharacters = i + 1;
+        }
+
+        
     }   
 }
