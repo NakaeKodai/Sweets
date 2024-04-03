50ec12bba64b306000f0ef7ce8c2d9f0e66fb12a
FUNA1116 <153796025+FUNA1116@users.noreply.github.com>
2024-04-04 02:00:38 +0900

    透明の壁の判定（未完成）

--- Assets/Script/Player/Move.cs
+++ Assets/Script/Player/Move.cs
@@ -11,7 +11,9 @@ public class Move : MonoBehaviour
     private Vector2 playerDirection; //プレイヤーの向き
     public byte speed = 5; //移動の速さ
     
-
+    public static bool tachWall = false; //壁に当たっているかを判別
+    // public static bool ta
+    [SerializeField]LayerMask WallLayer;
 
     void Start()
     {
@@ -21,14 +23,48 @@ public class Move : MonoBehaviour
 
     void Update()
     {
-
         playerInputAction.Player.Dash.performed += ctx => speed = 20;
         playerInputAction.Player.Dash.canceled += ctx => speed = 5;
 
         playerDirection = playerInputAction.Player.Move.ReadValue<Vector2>();
-        transform.Translate(
+        // 以下Raycastを利用した壁の判定（未完成）
+        Vector3 playerPosition = transform.position;
+        Vector3 rayDirection = playerDirection;
+        // RaycastHit wallHit = Physics.Raycast(playerPosition, rayDirection, 5f,WallLayer);
+        Ray ray = new Ray(playerPosition, rayDirection);
+        Debug.DrawRay(playerPosition,rayDirection,Color.blue,0.5f);
+        RaycastHit wallHit;
+        if(!Physics.Raycast(ray, out wallHit, 6.0f, WallLayer)){
+            transform.Translate(
             playerDirection.x * speed * Time.deltaTime,
             0.0f,
             playerDirection.y * speed * Time.deltaTime);
+        }
+        
+
+
     }
+
+    // Trigerを使用した判定（没）
+    // void OnTriggerEnter(Collider collider){
+    //     if(collider.tag == "Wall"){
+    //         // tachWall = true;
+    //         // Debug.Log("tachWallをオン");
+    //         speed = 0;
+    //     }
+    // }
+    // void OnTriggerStay(Collider collider){
+    //     if(collider.tag == "Wall" && speed != 0){
+    // //         tachWall = true;
+    // //         Debug.Log("tachWallをオン");
+    // speed = 0;
+    //     }
+    // }
+    // void OnTriggerExit(Collider collider){
+    //     if(collider.tag == "Wall"){
+    //         // tachWall = false;
+    //         // Debug.Log("tachWallをオフ");
+    //         // speed = 5;
+    //     }
+    // }
 }
