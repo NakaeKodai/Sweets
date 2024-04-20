ba3ddadcd97330c79369c9adef3a13ab08a2a893
NakaeKodai <0uz047526ky3k4d@au.com>
2024-04-20 19:57:34 +0900

    Animation

--- Assets/Script/Player/Move.cs
+++ Assets/Script/Player/Move.cs
@@ -10,6 +10,13 @@ public class Move : MonoBehaviour
 
     private Vector2 playerDirection; //プレイヤーの向き
     public byte speed = 5; //移動の速さ
+    public byte normalSpeed = 5;
+    public byte maxSpeed = 20;
+
+    private bool isWalk;
+    public bool isDush;
+
+    Animator animator;//アニメーションの変数
     
     [SerializeField]LayerMask WallLayer;
 
@@ -19,12 +26,40 @@ public class Move : MonoBehaviour
         playerInputAction.Enable();
     }
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        playerInputAction.Player.Dash.performed += ctx => speed = 20;
-        playerInputAction.Player.Dash.canceled += ctx => speed = 5;
+        // playerInputAction.Player.Dash.performed += ctx => speed = maxSpeed;
+        // playerInputAction.Player.Dash.canceled += ctx => speed = normalSpeed;
+        playerInputAction.Player.Dash.performed += ctx => {
+            speed = maxSpeed;
+            isDush = true;
+        };
+        
+        playerInputAction.Player.Dash.canceled += ctx => {
+            speed = normalSpeed;
+            isDush = false;
+        };
 
         playerDirection = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        if(playerDirection.x != 0 || playerDirection.y != 0)
+        {
+            isWalk = true;
+            animator.SetFloat("InputX",playerDirection.x);
+            animator.SetFloat("InputY",playerDirection.y);
+        }else
+        {
+            isWalk = false;
+        }
+        animator.SetBool("IsDush",isDush);
+        animator.SetBool("IsWalk",isWalk);
+
+        // if(playerDirection.x != 0 || playerDirection.y != 0)
 
         // 以下Raycastを利用した壁の判定
         Vector3 playerPosition = transform.position;//プレイヤーの位置を取得
