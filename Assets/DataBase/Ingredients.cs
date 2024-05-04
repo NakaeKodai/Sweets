4349809bc3f1846b8fec461351a8e9ed5d7d7692
NakaeKodai <0uz047526ky3k4d@au.com>
2024-05-04 19:09:16 +0900

    DB

---
+++ Assets/DataBase/Ingredients.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable] public class Ingredients
+{
+    public string name;
+    public Sprite image;
+}
