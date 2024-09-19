2b4665023857c0dcc3d3fd662a23dddce39731e7
FUNA1116 <153796025+FUNA1116@users.noreply.github.com>
2024-09-20 00:01:00 +0900

    ちょっとだけ

--- Assets/DataBase/SweetsList/sweetsRecipe.cs
+++ Assets/DataBase/SweetsList/sweetsRecipe.cs
@@ -12,6 +12,10 @@ using System;
     public bool canMake;//作成可能か否か(処理を楽にするため)
     public string infomation;//説明文
     public bool wishList = false;//ウィッシュリストに入っているかを判別する
+    [Header("味関連")]
+    public int 甘さ;//甘さの数値
+    public int 酸味;
+    public int 苦味;
     [Header("以下は使用する素材のIDと個数")]
     public List<recipeItem> materialsList = new List<recipeItem>();
 }
